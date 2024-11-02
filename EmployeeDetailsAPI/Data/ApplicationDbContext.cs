93387d5e9155c8c8ef8242d7d86854b2dea96b8f
USER <USER@DESKTOP-FTRCHEB>
2024-11-02 15:34:25 +0530

    Add project files.

---
+++ EmployeeDetailsAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeDetailsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeDetailsAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ EmployeeDetailsAPI/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeDetailsAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfHire { get; set; }
+    }
+}
